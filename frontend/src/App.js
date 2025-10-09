1b937a1cdceb4c586a4bb77555076b865886eece
swidom <u7834930@anu.edu.au>
2025-10-09 12:36:02 +1100

    Configure Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext"; 
+import { ThemeProvider } from "./context/ThemeContext";
+
+// user
 import WelcomePage from "./pages/user/WelcomePage";
 import ProfilePage from "./pages/user/ProfilePage";
 import WhyPage from "./pages/user/WhyPage";
@@ -9,20 +10,46 @@ import ThankYouPage from "./pages/user/ThankYouPage";
 import Layout from "./components/Layout";
 import LoginPage from "./pages/user/LoginPage";
 
+// admin
+import AdminLayout from "./components/AdminLayout";
+import DashBoard from "./pages/admin/DashBoard";
+import CreateNewIssue from "./pages/admin/CreateNewIssue";
+import IssueReport from "./pages/admin/IssueReport";
+import WhyReport from "./pages/admin/WhyReport";
+import HowReport from "./pages/admin/HowReport";
+import ProfileReport from "./pages/admin/ProfileReport";
+import EngagementReport from "./pages/admin/EngagementReport";
+import UserDataManagement from "./pages/admin/UserDataManagement";
+
 function App() {
     return (
         <ThemeProvider>
-        <Router>
-            <Routes>
-                <Route path="/" element={<Layout><WelcomePage /></Layout>} />
-                <Route path="/login" element={<Layout><LoginPage /></Layout>} />
-                <Route path="/profile" element={<Layout><ProfilePage /></Layout>} />
-                <Route path="/why" element={<Layout><WhyPage /></Layout>} />
-                <Route path="/how" element={<Layout><HowPage /></Layout>} />
-                <Route path="/thankyou" element={<Layout><ThankYouPage /></Layout>} />
-                <Route path="*" element={<Navigate to="/login" replace />} />
-            </Routes>
-        </Router>
+            <Router>
+                <Routes>
+                    {/* ---------- USER ROUTES ---------- */}
+                    <Route path="/" element={<Layout><WelcomePage /></Layout>} />
+                    <Route path="/login" element={<Layout><LoginPage /></Layout>} />
+                    <Route path="/profile" element={<Layout><ProfilePage /></Layout>} />
+                    <Route path="/why" element={<Layout><WhyPage /></Layout>} />
+                    <Route path="/how" element={<Layout><HowPage /></Layout>} />
+                    <Route path="/thankyou" element={<Layout><ThankYouPage /></Layout>} />
+
+                    {/* ---------- ADMIN ROUTES ---------- */}
+                    <Route path="/admin" element={<AdminLayout />}>
+                        <Route path="dashboard" element={<DashBoard />} />
+                        <Route path="new-issue" element={<CreateNewIssue />} />
+                        <Route path="issue-report" element={<IssueReport />} />
+                        <Route path="why-report" element={<WhyReport />} />
+                        <Route path="how-report" element={<HowReport />} />
+                        <Route path="profile-report" element={<ProfileReport />} />
+                        <Route path="engagement-report" element={<EngagementReport />} />
+                        <Route path="manage-users" element={<UserDataManagement />} />
+                    </Route>
+
+                    {/* ---------- DEFAULT ---------- */}
+                    <Route path="*" element={<Navigate to="/login" replace />} />
+                </Routes>
+            </Router>
         </ThemeProvider>
     );
 }
