05633c9897cc6596226da9fabff8ecb7fa128e88
Jingwen Zhang <u7900816@anu.edu.au>
2026-04-30 15:10:43 +1000

    Update CreateNewIssue.jsx

--- frontend/src/pages/admin/CreateNewIssue.jsx
+++ frontend/src/pages/admin/CreateNewIssue.jsx
@@ -21,7 +21,7 @@ export default function CreateNewIssue() {
 
   const API_BASE =
     process.env.REACT_APP_API_BASE_URL ||
-    'http://localhost:8080'
+    'https://codesign-project.onrender.com'
 
   const handleSubmit = async () => {
     const content = issueContent.trim()
